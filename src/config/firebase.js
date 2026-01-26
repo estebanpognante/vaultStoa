a229872d991ac663bd94451fe4c2047a2705a467
estebanpognante <estebanpognante@gmail.com>
2026-01-25 22:50:54 -0300

    Update firebase config to use env vars

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// TODO: Replace with your actual Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCV2LFS1PrQB_7d29IveR0nIeup1_LT_-Y",
-    authDomain: "console-9f291.firebaseapp.com",
-    projectId: "console-9f291",
-    storageBucket: "console-9f291.firebasestorage.app",
-    messagingSenderId: "634205139387",
-    appId: "1:634205139387:web:d570163690a2a4c0fa549e"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export default app;
